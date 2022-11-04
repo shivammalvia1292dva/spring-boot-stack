199718f426eba43f5de45a0dc31230ee54deee64
Shivam malvia <shivam.malvia1292dva@gmail.com>
2022-11-04 16:29:33 +0530

    Added support for DB

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -7,22 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class CurrencyExchangeRepository {
-
-    private  List<CurrencyExchange> list = new ArrayList<>();
-
-    CurrencyExchangeRepository(){
-        CurrencyExchange cx1 = new CurrencyExchange(10001l,"USD","INR",BigDecimal.valueOf(65));
-        CurrencyExchange cx2 = new CurrencyExchange(10002l,"EUR","INR",BigDecimal.valueOf(75));
-        CurrencyExchange cx3 = new CurrencyExchange(10003l,"AUD","INR",BigDecimal.valueOf(25));
-        list.add(cx1);
-        list.add(cx2);
-        list.add(cx3);
-    }
-    public CurrencyExchange findBy(String from, String to){
-        return list.stream().filter(cx -> cx.getFrom().equals(from) && cx.getTo().equals(to)).findFirst().get();
-    }
-    public List<CurrencyExchange> findAll(){
-        return list;
-    }
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
 }
