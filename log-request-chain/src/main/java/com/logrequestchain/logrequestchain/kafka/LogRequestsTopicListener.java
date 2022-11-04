22afb2d1dca3d6fcb734d0faab3287f94e95b7d8
unknown <shashiptale@gmail.com>
2022-11-04 17:10:46 +0530

    added kafka tracing support for otel

--- log-request-chain/src/main/java/com/logrequestchain/logrequestchain/kafka/LogRequestsTopicListener.java
+++ log-request-chain/src/main/java/com/logrequestchain/logrequestchain/kafka/LogRequestsTopicListener.java
@@ -1,5 +1,8 @@
 package com.logrequestchain.logrequestchain.kafka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,8 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 
 @Service
 public class LogRequestsTopicListener {
@@ -22,9 +28,37 @@ public class LogRequestsTopicListener {
     @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
     public void consume(ConsumerRecord<String, String> payload) {
         logger.info("log-request-chain-topic-consumer-called");
-        logger.info(payload.value());
-        String arr[] = payload.value().split(",");
-        list.add("Received request for conversion of  log-request consumer" + arr[0] + " to " + arr[1]);
+		Tracer tracer = GlobalOpenTelemetry.getTracerProvider().get("log-api-requests-chain");
+		Span parentSpan = Span.current();
+		logger.info("=====================================================================================");
+		logger.info("=====================================================================================");
+		logger.info(parentSpan.getSpanContext().getTraceId());
+		logger.info(parentSpan.getSpanContext().getSpanId());
+		logger.info("" + parentSpan.getSpanContext().isRemote());
+		logger.info("" + parentSpan.getSpanContext().isSampled());
+		logger.info("" + parentSpan.getSpanContext().isValid());
+
+
+		Span childSpan = tracer.spanBuilder("LogRequestsTopicListener_consume_span").setParent(Context.current().with(parentSpan)).startSpan();
+		logger.info("=====================================================================================");
+		logger.info("=====================================================================================");
+		logger.info(childSpan.getSpanContext().getTraceId());
+		logger.info(childSpan.getSpanContext().getSpanId());
+		logger.info("" + childSpan.getSpanContext().isRemote());
+		logger.info("" + childSpan.getSpanContext().isSampled());
+		logger.info("" + childSpan.getSpanContext().isValid());
+		// Make the span the current span
+		try (Scope scope = childSpan.makeCurrent()) {
+	        logger.info(payload.value());
+	        String arr[] = payload.value().split(",");
+	        
+	        list.add("Received request for conversion of  log-request consumer" + arr[0] + " to " + arr[1]);
+	    	scope.close();	
+		} finally {
+			childSpan.end();
+			parentSpan.end();
+		}
+
     }
 
     public List<String> getList() {
